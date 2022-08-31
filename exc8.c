b9a935aab789f1c495fed62dcc973e210079a125
HenriqueKrause <henriquebkrause2312@gmail.com>
2022-08-31 14:57:03 -0300

    exercicio slide2 PC

---
+++ exc8.c
@@ -0,0 +1,20 @@
+// Faça um programa que lê n números decimais, armazenando-os em um vetor. Após, o
+// programa identifica o menor número do vetor e imprime sua posição.
+
+#include <stdio.h>
+
+int main(){
+
+    int vetor[10], i, menor, posicao;
+
+    for(i = 0; i < 10; i++){
+        vetor[i]= 1;
+        if(vetor[i] < menor){
+            menor = vetor[i];
+            posicao = i;
+            }
+        }
+        return 0;
+}
+
+  
